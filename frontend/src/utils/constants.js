05dd0d12cb83eb7de04a2c794ba6c46dd83f6338
gitlab <arohansingh300@gmail.com>
2026-07-23 20:20:49 +0530

    Update

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://developers-hub-server.vercel.app";
+    : "https://developers-hub-zeta.vercel.app/";
